dcf80c0db4f62518ceff5a854aea7630b0581143
Soap <gorka.jabonero@estudioenpenascal.com>
2023-05-30 13:53:49 +0200

    Commit Inicial

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const btnPlatos = document.querySelector("#btn-cargar");
+const divPlatos = document.querySelector(".cargar-platos");
+
+let alergias = [];
+
+
+btnPlatos.addEventListener("click", () => {
+divPlatos.style.height= "80vh";    
+divPlatos.style.backgroundColor= "red";
+});
