6020a134efa0b9ae89bf6fe536be924758c4e340
alex23 <alexandergalvez880208@gmail.com>
2023-05-31 20:19:51 +0200

    js funcionando

--- js/main.js
+++ js/main.js
@@ -30,23 +30,59 @@
 
 ///////////////////////////////////////alex programation////////////////////////////////////////////////
 
-let hacerclic=document.querySelector('#clicplatos');
-let prua=document.getElementsByClassName('imagen');
+const hacerclic=document.querySelectorAll('.plato-filtro');
+const imagenvacia=document.querySelectorAll('.imagen');
+const valordeetiquetap=document.querySelectorAll('.mostrarvalor')
 
-var arrayimg=new Array;
+var arrayimg=new Array();
+let prua1;
+let insertarimg;
 
-hacerclic.addEventListener('click', () => {
-    for(let i=0; i < prua.length; i++){
-        prua[i].src=arrayimg[i]
-    }
-});
-  
-    for(let i= 0; i < arrayimg.length; i++){
-    }
+/////////////////////////////////////recoger valores de los menus////////////////////////////////////////
 
-    for (let i = 1; i<=16 ; i++){
-        arrayimg.push("/images/menus/plato"+" " +"(" + i + ").png") 
+    for(let i= 0; i < hacerclic.length; i++){
+        hacerclic[i].addEventListener ('click', () => {
+            prua1=valordeetiquetap[i].textContent
+            localStorage.setItem("contenido", JSON.stringify(prua1));
+            // localStorage.setItem("imagenes", JSON.stringify(arrayimg));
+            location.href='html/pagina-menus.html'
+          
+        }) 
+        }
+////////////////////////////insertar los menus en la pagina menus si se cumple la condicion////////////////////////
+
+        addEventListener("load" , empezar)
+        function empezar(){
+            let abc=JSON.parse(localStorage.getItem("contenido"));
+            insertarimg=abc;
+             
+        if(abc=='Sin alergias'){
+        insertar() 
     }
+    else{
+        console.log('hay probllemas')
+    }
+}
+/////////////////////// funcion para insertar los diferentes menus//////////////////////////////////////////////
+        function insertar(){
+            for(let i=0; i < imagenvacia.length; i++){
+                imagenvacia[i].src=arrayimg[i] 
+                }  
+        }
+
+////////////////////////////////////////////////recorriendo arrays/////////////////////////////////////////
+
+        for(let i= 0; i < valordeetiquetap.length; i++){
+        }
+    
+        for(let i= 0; i < arrayimg.length; i++){
+        }
+    
+        for (let i = 1; i<=16 ; i++){
+            arrayimg.push("/images/menus/plato"+" " +"(" + i + ").png") 
+        }
+
+
    
 
 
