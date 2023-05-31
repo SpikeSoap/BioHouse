21e9f22575843d844a45ccf162a029cd1e279b95
Soap <gorka.jabonero@estudioenpenascal.com>
2023-05-31 09:21:00 +0200

    Cambios estrutura

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 const btnPlatos = document.querySelector("#btn-cargar");
 const divPlatos = document.querySelector(".cargar-platos");
+// const foto1 = document.querySelector("#foto1");
 
-let alergias = [];
+
+let alergias = ["images/alergia1.png", "images/alergia2.png", "images/alergia3.png", "images/alergia4.png", "images/alergia5.png"];
 
 
 btnPlatos.addEventListener("click", () => {
-divPlatos.style.height= "80vh";    
-divPlatos.style.backgroundColor= "red";
+    divPlatos.style.height = "300vh";
+    divPlatos.style.width = "100%";
+
+
+    for (let i = 0; i < alergias.length; i++) {
+        
+        let imgFoto = document.createElement("img");
+        imgFoto.style.width = "18%";
+        imgFoto.id = "imagen" + i;
+        imgFoto.src = alergias[i];
+    }
+
 });
