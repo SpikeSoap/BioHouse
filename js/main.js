277d6b087fbcb16719e7147970040a3b4f54085e
yusra88 <yousra.rhazouali88@gmail.com>
2023-06-02 09:54:20 +0200

    9:45 yus

--- js/main.js
+++ js/main.js
@@ -35,6 +35,7 @@ let arrayimg=new Array();
 let varquerecogevalordelbotonmenu;
 let insertarimg;
 let arrayprecios=new Array()
+let arrayplatos=new Array()
 
 
 /////////////////////////////////////recoger valores de los menus////////////////////////////////////////
@@ -53,21 +54,27 @@ let arrayprecios=new Array()
         function empezar(){
             let abc=JSON.parse(localStorage.getItem("contenido"));
             insertarimg=abc;
-            asigplatos()
+
+            asigprecioplatos()
+            asigdescplatos()
+
             console.log(arrayprecios);
+            console.log(arrayplatos);
              
         if(abc=='Sin alergias'){
             for (let i = 1; i<=16 ; i++){
                 arrayimg.push("../images/sinalergias/plato"+" " +"(" + i + ").png") 
             }
         insertar() 
+        recorrerplatos()
+        recorrerprecios()
     }
     if (abc=='Sin gluten'){
         for (let i = 1; i<=16 ; i++){
             arrayimg.push("../images/singluten/sg" + i +".jpg") 
         }
         insertar() 
-       
+        recorrerplatos()
     }
     if (abc=='Sin lactosa'){
         for (let i = 1; i<=16 ; i++){
@@ -82,14 +89,14 @@ let arrayprecios=new Array()
 
         }
         insertar() 
-       
+        recorrerplatos()
     }
     if (abc=='Diabéticos'){
         for (let i = 1; i<=16 ; i++){
             arrayimg.push("../images/diabeticos/db" + i +".jpg") 
         }
         insertar() 
-       
+        recorrerplatos()
     }
 }
 /////////////////////// funcion para insertar los diferentes menus//////////////////////////////////////////////
@@ -124,32 +131,55 @@ let arrayprecios=new Array()
                 })
             }
 
-            /////////////////////////////////////////////arrays de objetos/////////////////////////////////////////
+            ////////////////////////////////////arrays de objetos yusra/////////////////////////////////////////
 
             
 let productos  = [ 
-    {nombre: "CHILI SIN CARNE (VEGGIE)",precio:10 }, 
-    {nombre: "LOMO DE BACALAO, CREMA DE SALMÓN AHUMADO, FLAN DE BRÓCOLI Y HABAS DE SOJA",precio:20 },  
-    {nombre: "ESTOFADO DE CARRILLERA DE TERNERA Y VERDURAS DE INVIERNO",precio:12 },  
-    {nombre: "FILETE DE SALMÓN, ARROZ BASMATI SALTEADO Y SALSA DE LIMÓN CONFITADO Y JENGIBRE",precio:15 },  
-    {nombre: "PENNE CON SALMÓN, BRÓCOLI, TOMATES SECOS, CREMA DE HINOJO",precio:10 },  
-    {nombre: "SURONCHU DEL MAR",precio:14 },  
-    {nombre: "DÚO DE ALBÓNDIGAS DE TERNERA CON ARROZ BLANCO Y SALVAJE Y COMPOTA DE TOMATE",precio:18 },  
-    {nombre: "FILETE DE POLLO MARINADO EN CURRY ROJO - LECHE DE COCO Y SU TRÍO DE ARROZ",precio:21 },  
-    {nombre: "LASAÑA DE DOS SALMONES",precio:13 },  
-    {nombre: "LOMO DE SALMÓN CON PIMENTÓN, DÚO DE LENTEJAS CON ZANAHORIA Y CEBOLLA",precio:10 },  
-    {nombre: "NOIX DE SAINT JACQUES, PIQUILLOS AUX LARDONS, SAUTÉ DE HARICOTS COCO & CHOU-FLEUR",precio:19 },  
-    {nombre: "FILETE DE POLLO, PURÉ DE PATATAS Y ESPINACAS",precio:22 },
-    {nombre: "TAGINE DE VERDURAS MARROQUÍ CON ALBÓNDIGAS DE TERNERA",precio:27},
-    {nombre: "PAELLA CON POLLO Y MARISCOS",precio:25 },
-    {nombre: "ARROZ CON POLLO",precio:16 },
-    {nombre: "FILETE DE POLLO, CALABAZA INFUSIONADA CON ANÍS ESTRELLADO Y CASTAÑAS.",precio:12 },
-    {nombre: "PARMENTIER DE PALETILLA DE CORDERO Y BONIATO, DÁTILES Y NUECES, SALSA DE SÉSAMO NEGRO",precio:29 },
-    {nombre: "VIEIRAS, PIMI TOS DE BACON, COCO SALTEADO Y COLIFLOR",precio:17 },
+    {nombre: "CHILI SIN CARNE (VEGGIE)",precio:'10.99' }, 
+    {nombre: "LOMO DE BACALAO, CREMA DE SALMÓN AHUMADO, FLAN DE BRÓCOLI Y HABAS DE SOJA",precio:'20.99' },  
+    {nombre: "ESTOFADO DE CARRILLERA DE TERNERA Y VERDURAS DE INVIERNO",precio:'12.00' },  
+    {nombre: "FILETE DE SALMÓN, ARROZ BASMATI SALTEADO Y SALSA DE LIMÓN CONFITADO Y JENGIBRE",precio:'15.00' },  
+    {nombre: "PENNE CON SALMÓN, BRÓCOLI, TOMATES SECOS, CREMA DE HINOJO",precio:'10.99' },  
+    {nombre: "SURONCHU DEL MAR",precio:'14.99' },  
+    {nombre: "DÚO DE ALBÓNDIGAS DE TERNERA CON ARROZ BLANCO Y SALVAJE Y COMPOTA DE TOMATE",precio:'18.99' },  
+    {nombre: "FILETE DE POLLO MARINADO EN CURRY ROJO - LECHE DE COCO Y SU TRÍO DE ARROZ",precio:'21.99' },  
+    {nombre: "LASAÑA DE DOS SALMONES",precio:'13.00' },  
+    {nombre: "LOMO DE SALMÓN CON PIMENTÓN, DÚO DE LENTEJAS CON ZANAHORIA Y CEBOLLA",precio:'10.00' },  
+    {nombre: "NOIX DE SAINT JACQUES, PIQUILLOS AUX LARDONS, SAUTÉ DE HARICOTS COCO & CHOU-FLEUR",precio:'19.00' },  
+    {nombre: "FILETE DE POLLO, PURÉ DE PATATAS Y ESPINACAS",precio:'22.50' },
+    {nombre: "TAGINE DE VERDURAS MARROQUÍ CON ALBÓNDIGAS DE TERNERA",precio:'12.99'},
+    {nombre: "PAELLA CON POLLO Y MARISCOS",precio:'25.00' },
+    {nombre: "helado de fresas",precio:'16.00' },
+    {nombre: "FILETE DE POLLO, CALABAZA INFUSIONADA CON ANÍS ESTRELLADO Y CASTAÑAS.",precio:'12.99' },
+    {nombre: "PARMENTIER DE PALETILLA DE CORDERO Y BONIATO, DÁTILES Y NUECES, SALSA DE SÉSAMO NEGRO",precio:'29.99' },
+    {nombre: "VIEIRAS, PIMI TOS DE BACON, COCO SALTEADO Y COLIFLOR",precio:'17.00' },
     ] 
 
-    function asigplatos(){   
-        let select= productos.filter(a => a.precio >'10'& a.precio<'17');
+    function asigprecioplatos(){   
+        let select= productos.filter(a => a.precio >'0.00');
         for(let i=0; i<select.length; i++){
-         arrayprecios.push (select[i].nombre)
+         arrayprecios.push (select[i].precio)
      }} 
+
+     function asigdescplatos(){   
+        let select= productos.filter(a => a.nombre >'0');
+        for(let i=0; i<select.length; i++){
+         arrayplatos.push (select[i].nombre)
+     }} 
+
+     let tituloplatos = document.querySelectorAll(".desc-menu");
+     let preciosMenu = document.querySelectorAll(".precio")
+    let divisa=' €'
+
+     function recorrerplatos(){
+        for(let i=0; i<tituloplatos.length; i++){
+        tituloplatos[i].innerHTML = arrayplatos[i]
+        }
+     }
+
+     function recorrerprecios(){
+        for(let i=0; i<preciosMenu.length; i++){
+            preciosMenu[i].innerHTML = arrayprecios[i]+divisa
+        }
+     }
+     
