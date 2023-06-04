6aa7c86f9a5801cb3a272c9ea8b69d3d2c574068
alex23 <alexandergalvez880208@gmail.com>
2023-06-04 20:58:19 +0200

    modificacion de precios

--- js/main.js
+++ js/main.js
@@ -61,6 +61,8 @@ const tituloplatos = document.querySelectorAll(".desc-menu");
 const preciosMenu = document.querySelectorAll(".precio");
 const recorrercarrito = document.getElementById("carrito");
 const shopping = document.querySelector(".menu-shop")
+const totalgeneral=document.getElementById('monto-total')
+
 
 
 
@@ -157,14 +159,17 @@ let cantidaddeplatosseleccionados;
             for(let i= 0; i < arrayimg.length; i++){
             }
         })
-       
+
+      //////////////enviar al carro, coger src de imagen, envian precio al span /////////////////////
+     
       
             for(let i= 0; i < clicimagen.length; i++){
                 clicimagen[i].addEventListener("click", () => {
                        imagendelbotonmenu=clicimagen[i].src;
                        cantidaddeplatosseleccionados=preciosMenu[i].textContent
-                
-    
+                     
+
+
                     let contcarrito=document.getElementById('carrito')
                     let visualizar=document.getElementById('inf-primerafila-cart')
                     
@@ -175,8 +180,7 @@ let cantidaddeplatosseleccionados;
                     if( contcarrito.style.width=="0%" ){
                         contcarrito.style.visibility="visible"
                         contcarrito.style.width="20%"
-                        visualizar.style.display="block"
-                        
+                        visualizar.style.display="block" 
                     }
 
                     createcarro()
@@ -191,22 +195,22 @@ let cantidaddeplatosseleccionados;
 let productos  = [ 
     {nombre: "CHILI SIN CARNE (VEGGIE)",precio:'10.99' }, 
     {nombre: "LOMO DE BACALAO, CREMA DE SALMÓN AHUMADO, FLAN DE BRÓCOLI Y HABAS DE SOJA",precio:'20.99' },  
-    {nombre: "ESTOFADO DE CARRILLERA DE TERNERA Y VERDURAS DE INVIERNO",precio:'12.00' },  
-    {nombre: "FILETE DE SALMÓN, ARROZ BASMATI SALTEADO Y SALSA DE LIMÓN CONFITADO Y JENGIBRE",precio:'15.00' },  
+    {nombre: "ESTOFADO DE CARRILLERA DE TERNERA Y VERDURAS DE INVIERNO",precio:'12.10' },  
+    {nombre: "FILETE DE SALMÓN, ARROZ BASMATI SALTEADO Y SALSA DE LIMÓN CONFITADO Y JENGIBRE",precio:'15.20' },  
     {nombre: "PENNE CON SALMÓN, BRÓCOLI, TOMATES SECOS, CREMA DE HINOJO",precio:'10.99' },  
     {nombre: "SURONCHU DEL MAR",precio:'14.99' },  
     {nombre: "DÚO DE ALBÓNDIGAS DE TERNERA CON ARROZ BLANCO Y SALVAJE Y COMPOTA DE TOMATE",precio:'18.99' },  
     {nombre: "FILETE DE POLLO MARINADO EN CURRY ROJO - LECHE DE COCO Y SU TRÍO DE ARROZ",precio:'21.99' },  
-    {nombre: "LASAÑA DE DOS SALMONES",precio:'13.00' },  
-    {nombre: "LOMO DE SALMÓN CON PIMENTÓN, DÚO DE LENTEJAS CON ZANAHORIA Y CEBOLLA",precio:'10.00' },  
-    {nombre: "NOIX DE SAINT JACQUES, PIQUILLOS AUX LARDONS, SAUTÉ DE HARICOTS COCO & CHOU-FLEUR",precio:'19.00' },  
-    {nombre: "FILETE DE POLLO, PURÉ DE PATATAS Y ESPINACAS",precio:'22.50' },
+    {nombre: "LASAÑA DE DOS SALMONES",precio:'13.60' },  
+    {nombre: "LOMO DE SALMÓN CON PIMENTÓN, DÚO DE LENTEJAS CON ZANAHORIA Y CEBOLLA",precio:'10.70' },  
+    {nombre: "NOIX DE SAINT JACQUES, PIQUILLOS AUX LARDONS, SAUTÉ DE HARICOTS COCO & CHOU-FLEUR",precio:'19.90' },  
+    {nombre: "FILETE DE POLLO, PURÉ DE PATATAS Y ESPINACAS",precio:'22.55' },
     {nombre: "TAGINE DE VERDURAS MARROQUÍ CON ALBÓNDIGAS DE TERNERA",precio:'12.99'},
-    {nombre: "PAELLA CON POLLO Y MARISCOS",precio:'25.00' },
-    {nombre: "helado de fresas",precio:'16.00' },
+    {nombre: "PAELLA CON POLLO Y MARISCOS",precio:'24.90' },
+    {nombre: "helado de fresas",precio:'16.30' },
     {nombre: "FILETE DE POLLO, CALABAZA INFUSIONADA CON ANÍS ESTRELLADO Y CASTAÑAS.",precio:'12.99' },
     {nombre: "PARMENTIER DE PALETILLA DE CORDERO Y BONIATO, DÁTILES Y NUECES, SALSA DE SÉSAMO NEGRO",precio:'29.99' },
-    {nombre: "VIEIRAS, PIMI TOS DE BACON, COCO SALTEADO Y COLIFLOR",precio:'17.00' },
+    {nombre: "VIEIRAS, PIMI TOS DE BACON, COCO SALTEADO Y COLIFLOR",precio:'17.150' },
     ] 
 
     function asigprecioplatos(){   
@@ -230,27 +234,26 @@ let productos  = [
 
      function recorrerprecios(){
         for(let i=0; i<preciosMenu.length; i++){
-            preciosMenu[i].innerHTML = arrayprecios[i]+' '+ '€'
+            preciosMenu[i].innerHTML = arrayprecios[i]
         }
      }
     
 
-
-     
      ////////carrito crear y eliminar/////////
      
-
      function createcarro(){
-      let imagen
-      let pasta
-      let cantidad
-      let borrar
-      let div2
-
-        let div = document.createElement("div")
+        let div
+        let imagen
+        let pasta
+        let cantidad
+        let borrar
+        let div2
+        let opciones
+        
+        
+        div = document.createElement("div")
         div.className = "product-shop"
         shopping.appendChild(div)
-
 
         imagen =document.createElement("img")
         imagen.className = "img-select"
@@ -259,25 +262,25 @@ let productos  = [
 
         pasta = document.createElement("span")
         pasta.className = "precio-art-carrito"
-        pasta.textContent = cantidaddeplatosseleccionados
+        pasta.textContent = cantidaddeplatosseleccionados+'€'
         div.appendChild(pasta)
 
         div2 = document.createElement("div")
         div2.className = "botones"
         div.appendChild(div2)
  
-     
-    cantidad = document.createElement("select");
-    cantidad.className = "Cant-prod";
-    cantidad.textContent = '';
-    div2.appendChild(cantidad);
+        cantidad = document.createElement("select");
+        cantidad.className = "Cant-prod";
+        cantidad.textContent = '';
+        div2.appendChild(cantidad);
 
-    for (let i = 0; i <= 5; i++) {
-        let option = document.createElement("option");
-        option.className = "cant-opt" + i;
-        option.value = i;
-        option.textContent = i;
-        cantidad.appendChild(option);
+    for (let i = 1; i <= 5; i++) {
+        opciones = document.createElement("option");
+        opciones.className = "cant-opt";
+        opciones.value = i;
+        opciones.textContent = i;
+        cantidad.appendChild(opciones);
+
     }
 
        borrar=document.createElement('img')
@@ -286,9 +289,26 @@ let productos  = [
        div2.appendChild(borrar)
 
 
+       /////////////////funcion donde borrar y coger valores del carro, no funciona en mas ningun lado//////////
+       
+      let preciodelspandelcarrito=new Array()
        carrito.addEventListener('click', e =>{
-        if(e.target==borrar)
-        shopping.removeChild(div)
-    })
-}
 
+        if(e.target==borrar){
+            shopping.removeChild(div)
+        }
+        if(e.target==cantidad){
+            multiplicar()
+        }
+        pasta.textContent= preciodelspandelcarrito;
+    })
+    function multiplicar(){
+        preciodelspandelcarrito.push(parseFloat(cantidad.value)*parseFloat(pasta.textContent)) 
+    }
+}
+addEventListener('load', e =>{
+ 
+        console.log(e.target.pasta)  
+    
+ 
+})
